9df1a9afca534b317ae41b09d155025e0b31552a
EugeneReznik16051986 <50756243+EugeneReznik16051986@users.noreply.github.com>
2019-07-18 03:19:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+"use strict";
+
+// № 1
+let a = 3;
+if(a == '1'){
+    console.log('Верно');
+}
+else{
+console.log('Неверно');
+}
+
+
+// № 2
+let item = false; 
+if (item != true) { 
+    console.log('Верно'); 
+}
+ else { 
+    console.log('Неверно'); 
+} 
+    
+//Короткая
+item != true ? console.log('Верно') : console.log('Неверно');
+
+
+// № 3 
+let b = 5; 
+if (b > 0, b < 4) { 
+    console.log('Верно'); 
+}
+else { 
+    console.log('Неверно'); 
+} 
+
+
+// № 4
+let c = 4; 
+let d = 20; 
+if (c > 3, c < 12 || d >= 7, d < 15) { 
+    console.log('Верно'); 
+}
+else { 
+    console.log('Неверно'); 
+}
+
+
+
+// № 5
+let month = 7;
+if (month == 1 || month == 2 || month == 12){
+    console.log('Зима');
+}		
+else if (month >= 6, month < 9){
+    console.log('Лето');
+}
+else if (month >= 3 , month < 6){
+    console.log('Весна');
+}
+else if (month >= 9, month < 12){
+    console.log('Осень');
+}
+else{
+    console.log('Неверная дата');
+}
+    
+
+// № 6
+let i = 0; 
+while (i <= 100){
+    console.log(i);
+    i +=2; 
+} 
